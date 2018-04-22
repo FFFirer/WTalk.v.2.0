38d7c4a545514d0db4a16e39385eba6fe50cd253
明高 王 <0sdf0@sina.cn>
2018-04-22 15:36:26 +0800

    添加项目文件。

---
+++ WTalk.Helpers/DataHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTalk.Helpers
+{
+    public abstract class DataHelpers
+    {
+        public static void Handle(string data)
+        {
+            string[] split = data.Split(':');
+            switch (split[0])
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ WTalk.Helpers/TCPHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WTalk.Helpers
+{
+    public class TCPHelper
+    {
+        public IPAddress IP { get; set; }
+        public int port { get; set; }
+        public TcpClient tcpClient { get; set; }
+        public BinaryReader br { get; set; }
+        public BinaryWriter bw { get; set; }
+
+        public TCPHelper()
+        {
+            port = 51888;
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(var ip in ips)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = ip;
+                    break;
+                }
+            }
+        }
+
+        //指定IP地址
+        public TCPHelper(IPAddress ip, int port)
+        {
+            this.IP = ip;
+            this.port = port;
+        }
+        public void ConnectServer()
+        {
+            try
+            {
+                tcpClient = new TcpClient(IP.ToString(), port);
+                NetworkStream stream = tcpClient.GetStream();
+                br = new BinaryReader(stream);
+                bw = new BinaryWriter(stream);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Send2Server(string msg)
+        {
+            if (bw != null)
+            {
+                try
+                {
+                    bw.Write(msg);
+                    bw.Flush();
+                }
+                catch(Exception e)
+                {
+                    throw e;//异常处理
+                }
+            }
+        }
+    }
+}
